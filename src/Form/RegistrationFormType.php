0aeda047cb172db1f526dcc7e34637a700431e99
Meddy Cooper <meddycooper@outlook.com>
2025-09-25 14:31:52 +0100

    Update RegistrationFormType.php

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,17 +2,18 @@
 
 namespace App\Form;
 
-use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Validator\Constraints\File;
 
 class RegistrationFormType extends AbstractType
@@ -20,9 +21,17 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'label' => 'Email Address',
+                'attr' => [
+                    'placeholder' => 'Enter your email',
+                ],
+            ])
             ->add('username', TextType::class, [
                 'label' => 'Username',
+                'attr' => [
+                    'placeholder' => 'Choose a username',
+                ],
                 'required' => true,
             ])
             ->add('profilePicture', FileType::class, [
@@ -33,42 +42,45 @@ class RegistrationFormType extends AbstractType
                     new File([
                         'maxSize' => '2M',
                         'mimeTypes' => ['image/jpeg', 'image/png'],
-                        'mimeTypesMessage' => 'Please upload a valid image file',
-                    ])
+                        'mimeTypesMessage' => 'Please upload a valid image file (JPEG/PNG)',
+                    ]),
                 ],
             ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'You must agree to the terms.',
                     ]),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password', 'placeholder' => 'Enter your password'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
+                        'minMessage' => 'Your password must be at least {{ limit }} characters long',
                         'max' => 4096,
                     ]),
                 ],
             ])
-        ;
+            ->add('register', SubmitType::class, [
+                'label' => 'Register',
+                'attr' => [
+                    'class' => 'btn btn-primary',
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
+        // Safe for public sharing, no entity tied
         $resolver->setDefaults([
-            'data_class' => User::class,
+            'data_class' => null,
         ]);
     }
 }
