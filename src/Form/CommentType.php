733742a3dd868417491d753f75560971598aa6bd
Meddy Cooper <meddycooper@outlook.com>
2025-09-25 14:30:30 +0100

    Update CommentType.php

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -1,7 +1,7 @@
 <?php
+
 namespace App\Form;
 
-use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -12,15 +12,19 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('content', TextareaType::class, [
-            'label' => 'Your Comment',
-            'attr' => ['rows' => 3],
+            'label' => 'Comment',
+            'attr' => [
+                'rows' => 3,
+                'placeholder' => 'Write your comment here',
+            ],
         ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
+        // Removed reference to specific entity to make this generic and public-friendly
         $resolver->setDefaults([
-            'data_class' => Comment::class,
+            'data_class' => null,
         ]);
     }
 }
