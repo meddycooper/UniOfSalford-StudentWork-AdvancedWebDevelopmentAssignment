0099fc60a6ec4bbdd1d3ef5248f4ed94271adb65
Meddy Cooper <meddycooper@outlook.com>
2025-09-25 14:20:06 +0100

    Update ReviewVote.php

--- src/Entity/ReviewVote.php
+++ src/Entity/ReviewVote.php
@@ -6,33 +6,29 @@ use App\Repository\ReviewVoteRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ReviewVoteRepository::class)]
-#[ORM\Table(name: "review_vote", uniqueConstraints: [
-    new ORM\UniqueConstraint(name: "unique_user_review", columns: ["user_id", "review_id"])
+#[ORM\Table(name: 'review_vote', uniqueConstraints: [
+    new ORM\UniqueConstraint(name: 'unique_user_review', columns: ['user_id', 'review_id'])
 ])]
 class ReviewVote
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Id, ORM\GeneratedValue, ORM\Column(type: 'integer')]
     private ?int $id = null;
 
-    #[ORM\ManyToOne(inversedBy: 'reviewVotes')]
+    #[ORM\ManyToOne(targetEntity: Review::class, inversedBy: 'reviewVotes')]
+    #[ORM\JoinColumn(nullable: false)]
     private ?Review $review = null;
 
-    #[ORM\ManyToOne(inversedBy: 'reviewVotes')]
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'reviewVotes')]
     #[ORM\JoinColumn(nullable: false)]
     private ?User $user = null;
+
+    // -------------------------
+    // Getters and Setters
+    // -------------------------
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function setId(int $id): static
-    {
-        $this->id = $id;
-
-        return $this;
     }
 
     public function getReview(): ?Review
@@ -43,9 +39,9 @@ class ReviewVote
     public function setReview(?Review $review): static
     {
         $this->review = $review;
-
         return $this;
     }
+
     public function getUser(): ?User
     {
         return $this->user;
