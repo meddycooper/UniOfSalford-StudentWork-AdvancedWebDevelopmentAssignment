6f4387185c3f0c559665fb6ca54f61a5cc79bcd5
Meddy Cooper <meddycooper@outlook.com>
2025-09-25 14:31:04 +0100

    Update ProfileType.php

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -2,11 +2,11 @@
 
 namespace App\Form;
 
-use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
@@ -18,14 +18,24 @@ class ProfileType extends AbstractType
         $builder
             ->add('username', TextType::class, [
                 'label' => 'Username',
+                'attr' => [
+                    'placeholder' => 'Enter your username',
+                ],
             ])
             ->add('bio', TextareaType::class, [
                 'label' => 'Bio',
                 'required' => false,
+                'attr' => [
+                    'placeholder' => 'Tell us about yourself',
+                    'rows' => 4,
+                ],
             ])
             ->add('location', TextType::class, [
                 'label' => 'Location',
                 'required' => false,
+                'attr' => [
+                    'placeholder' => 'Enter your location',
+                ],
             ])
             ->add('profilePicture', FileType::class, [
                 'label' => 'Profile Picture (JPEG or PNG)',
@@ -34,20 +44,24 @@ class ProfileType extends AbstractType
                 'constraints' => [
                     new File([
                         'maxSize' => '2M',
-                        'mimeTypes' => [
-                            'image/jpeg',
-                            'image/png',
-                        ],
+                        'mimeTypes' => ['image/jpeg', 'image/png'],
                         'mimeTypesMessage' => 'Please upload a valid image (JPEG or PNG)',
                     ]),
+                ],
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Save Profile',
+                'attr' => [
+                    'class' => 'btn btn-primary',
                 ],
             ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
+        // Generic form safe for public sharing
         $resolver->setDefaults([
-            'data_class' => User::class,
+            'data_class' => null,
         ]);
     }
 }
