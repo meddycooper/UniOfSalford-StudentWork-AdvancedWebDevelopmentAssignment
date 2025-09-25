a4225fdfdb1325d15ccd81c3288cf012bece253f
Meddy Cooper <meddycooper@outlook.com>
2025-09-25 14:18:37 +0100

    Update Favorite.php

--- src/Entity/Favorite.php
+++ src/Entity/Favorite.php
@@ -10,9 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: FavoriteRepository::class)]
 class Favorite
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Id, ORM\GeneratedValue, ORM\Column(type: 'integer')]
     private ?int $id = null;
 
     #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'favorites')]
@@ -23,31 +21,49 @@ class Favorite
     #[ORM\JoinColumn(nullable: false)]
     private ?Book $book = null;
 
+    // -------------------------
+    // Getters and Setters
+    // -------------------------
+
+    /**
+     * Get the ID of the favorite
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
+
+    /**
+     * Get the user who favorited the book
+     */
     public function getUser(): ?User
     {
         return $this->user;
     }
 
-    public function setUser(?User $user): self
+    /**
+     * Set the user who favorited the book
+     */
+    public function setUser(?User $user): static
     {
         $this->user = $user;
-
         return $this;
     }
 
+    /**
+     * Get the book that was favorited
+     */
     public function getBook(): ?Book
     {
         return $this->book;
     }
 
-    public function setBook(?Book $book): self
+    /**
+     * Set the book that was favorited
+     */
+    public function setBook(?Book $book): static
     {
         $this->book = $book;
-
         return $this;
     }
 }
