20f0b2a1496a9e3d82baad7d05b59efc917e4db6
Meddy Cooper <meddycooper@outlook.com>
2025-09-25 14:07:18 +0100

    Update BookApiController.php

--- src/Controller/BookApiController.php
+++ src/Controller/BookApiController.php
@@ -3,7 +3,6 @@
 
 namespace App\Controller;
 
-use GuzzleHttp\Client;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,66 +16,49 @@ class BookApiController extends AbstractController
     {
         $searchTerm = $request->query->get('q', '');
         $filter = $request->query->get('filter', '');
-        if (!$searchTerm) {
-            return $this->render('book_api/search.html.twig', [
-                'books' => [],
-                'query' => '',
-                'filter' => $filter,
-            ]);
-        }
-        $queryParam = $filter ? $filter . ':' . $searchTerm : $searchTerm;
-        $client = new Client();
-        $response = $client->request('GET', 'https://www.googleapis.com/books/v1/volumes', [
-            'query' => [
-                'q' => $queryParam,
-                'key' => 'my key',
-                'maxResults' => 10,
-            ]
-        ]);
-        $data = json_decode($response->getBody(), true);
+
+        // For portfolio purposes, we mock book search results
         $books = [];
-        if (isset($data['items'])) {
-            foreach ($data['items'] as $item) {
-                $volumeInfo = $item['volumeInfo'];
-                $books[] = [
-                    'title' => $volumeInfo['title'] ?? 'No title',
-                    'authors' => $volumeInfo['authors'] ?? [],
-                    'thumbnail' => $volumeInfo['imageLinks']['thumbnail'] ?? null,
-                    'description' => $volumeInfo['description'] ?? '',
-                ];
-            }
+        if ($searchTerm) {
+            $books = [
+                [
+                    'title' => 'Sample Book 1',
+                    'authors' => ['Author A'],
+                    'thumbnail' => null,
+                    'description' => 'This is a sample description for Book 1.',
+                ],
+                [
+                    'title' => 'Sample Book 2',
+                    'authors' => ['Author B'],
+                    'thumbnail' => null,
+                    'description' => 'This is a sample description for Book 2.',
+                ],
+            ];
         }
+
         return $this->render('book_api/search.html.twig', [
             'books' => $books,
             'query' => $searchTerm,
             'filter' => $filter,
         ]);
     }
+
     #[Route('/api/book/autocomplete', name: 'book_autocomplete', methods: ['GET'])]
     public function autocomplete(Request $request): JsonResponse
     {
         $query = $request->query->get('q', '');
-        if (strlen($query) < 2) {
-            return $this->json([]);
-        }
-        $client = new Client();
-        $response = $client->request('GET', 'https://www.googleapis.com/books/v1/volumes', [
-            'query' => [
-                'q' => 'intitle:' . $query,
-                'key' => 'AIzaSyADCFpacAi2WExIWu1QpZfkaZcXp5P869Q',
-                'maxResults' => 5,
-            ]
-        ]);
-        $data = json_decode($response->getBody(), true);
+        
+        // Normally this would call Google Books API using Guzzle
+        // Here we just return mock suggestions for portfolio purposes
         $suggestions = [];
-        if (!empty($data['items'])) {
-            foreach ($data['items'] as $item) {
-                $title = $item['volumeInfo']['title'] ?? '';
-                if ($title) {
-                    $suggestions[] = $title;
-                }
-            }
+        if (strlen($query) >= 2) {
+            $suggestions = [
+                $query . ' Sample Title 1',
+                $query . ' Sample Title 2',
+                $query . ' Sample Title 3',
+            ];
         }
+
         return $this->json($suggestions);
     }
 }
